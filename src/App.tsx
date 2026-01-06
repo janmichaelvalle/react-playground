2576362b616ea3e4a2d8114c956a2c00818a2e51
Mikee Valle <janmichaelvalle@gmail.com>
2026-01-07 00:00:42 +0800

    Initial react playground

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Profile } from "./Profile"
+
+function Gallery() {
+
+
+  return (
+    <>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </>
+  )
+}
+
+export default Gallery
