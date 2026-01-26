f55bda631bbcf526bf051ee2812f1bb3e6bf0b31
Mikee Valle <janmichaelvalle@gmail.com>
2026-01-26 19:41:39 +0800

    1-26-2026 session

--- src/pages/BreedersPage.tsx
+++ src/pages/BreedersPage.tsx
@@ -14,12 +14,90 @@ export function BreedersPage() {
   // Breeders is array of objects
   const [breeders, setBreeders] = React.useState<Breeders[]>(initialBreeders)
 
-
   // Should this be here?
   const [breederToUpdate, setBreederToUpdate] = React.useState<Breeder | null>(null)
   const [showEditModal, setshowEditModal] = React.useState(false)
   const [showDeleteModal, setShowDeleteModal] = React.useState(false)
   const [breederToDelete, setBreederToDelete] = React.useState<Breeder | null>(null)
+
+
+  React.useEffect(
+    () => {
+      // When working with Promise, you wont know WHEN exactly it will get resolved   
+
+      // Syntax callback:
+      // fetch('https://dummyjson.com/users/1')
+      //   .then((res) => res.json())
+      //   .then((data) => { 
+      //     setUser(data) 
+      //   });
+
+      // Syntax await (to avoid callback hell):
+      const fetchUser = async () => {
+        const res = await fetch('https://dummyjson.com/users/1')
+        const data = await res.json()
+        setUser(data)
+       
+        
+      }
+
+      fetchUser() // do not forget to call the function
+    },
+    []
+  )
+
+  const [user, setUser] = React.useState<{ firstName: string }>()
+
+
+  // React.useEffect(
+  //   () => {
+  //     document.title = "HEY"; // side-effect
+  //   }, 
+  //   [] // empty === initial render
+  // )
+
+  // // Invalid use of useEffect. Why???
+  // React.useEffect(
+  //   () => {
+  //     // Should only update page's title when breeders list actually updates
+  //     document.title = "THIS WILL RUN WHEN THE DEPS UPDATE";  // not totally side effect. Depends ON WHEN you actually want to it
+
+  //     // even for fetching data, it will ONLY be considered as effect based on WHEN YOU WANT TO FETCH the data
+  //     const res = await fetch(thirdPartyServiceUrl)
+  //     const data = await res.json()
+  //   }, 
+  //   [breeders] // if there's a deps === initial render + every time the deps updates
+  // )
+
+  // // Synchronize breedersCount state to breeders state
+  // React.useEffect(
+  //   () => {
+  //     setBreedersCount(breeders.length)
+  //   },
+  //   [breeders]
+  // )
+
+  // // - initial load
+  // // - subscribing to event/external api
+  // // - during clean ups (desctruction) 
+
+  // // React.useEffect(
+  // //   () => {
+  // //     // subscribe to 3rd partty api where data is provided
+  // //     realtimeDashboard.subscribe(
+  // //       () => {
+  // //         // synchronize your app
+  // //       }
+  // //     )
+
+  // //     // Clean up function: Will run upon component destruction
+  // //     return () => {
+  // //       document.title = "PAGE SWITCHED";
+  // //       realtimeDashboard.unsubscribe()
+  // //     }
+  // //   },
+  // //   []
+  // // )
 
 
   const addButtonClick = (event: React.MouseEvent) => {
@@ -46,6 +124,7 @@ export function BreedersPage() {
 
 
   const editButtonClick = (breederId: string) => {
+
     // Handle all edit logic here...
 
     setshowEditModal(true)
@@ -87,6 +166,7 @@ export function BreedersPage() {
 
   return (
     <>
+      <h1>{user && user.firstName}</h1>
       <h1>Breeders</h1>
       <BreedersTable
         breeders={breeders}
